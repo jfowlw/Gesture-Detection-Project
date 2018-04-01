32d939a5376309920dd6de89e153e1ab772d20bc
jrhill95 <32690216+jrhill95@users.noreply.github.com>
2018-04-01 19:02:33 -0400

    added wave capture

--- CS 440 Project 2/CS440_640_Lab6_part2/CS440_640_Lab6_part2/Source.cpp
+++ CS 440 Project 2/CS440_640_Lab6_part2/CS440_640_Lab6_part2/Source.cpp
@@ -46,12 +46,18 @@ void mySkinDetect(Mat& src, Mat& dst);
 
 void myFrameDifferencing(Mat& prev, Mat& curr, Mat& dst);
 
-bool myTemplateMatching(Mat& mainFrame, Mat& testFrame, Mat& templt, Mat& results);
+void myMotionEnergy(vector<Mat> mh, Mat& dst);
 
+bool myTemplateMatching(Mat& mainFrame, Mat& testFrame, Mat& templt, Mat& results);
 vector<Mat> createTemplates(Mat& image);
 
 //global 
+int frameSkipNum = 5;
 Mat okay = imread("okay-hand.jpg", IMREAD_COLOR); // Read the file
+Mat peace = imread("peacesign.jpg");
+Mat wave1 = imread("wave1.jpg", IMREAD_COLOR);
+Mat wave2 = imread("wave2.jpg", IMREAD_COLOR);
+Mat wave3 = imread("wave3.jpg", IMREAD_COLOR);
 
 int main()
 {
@@ -71,11 +77,22 @@ int main()
 	}
 
 	vector<Mat> templates = createTemplates(okay);
+	vector<Mat> peaceTemp = createTemplates(peace);
+	vector<Mat> wave1Temp = createTemplates(wave1);
+	vector<Mat> wave2Temp = createTemplates(wave2);
+	vector<Mat> wave3Temp = createTemplates(wave3);
+
+	templates.insert(templates.end(), peaceTemp.begin(), peaceTemp.end());
+	templates.insert(templates.end(), wave1Temp.begin(), wave1Temp.end());
+	templates.insert(templates.end(), wave2Temp.begin(), wave2Temp.end());
+	templates.insert(templates.end(), wave3Temp.begin(), wave3Temp.end());
+
+
 	//imshow("templ", templ);
 	//create a window called "MyVideo"
 	namedWindow("MyVideo0", WINDOW_AUTOSIZE);
 	RNG rng(12345);
-
+	//imshow("hand shape",okay);
 
 	// read a backgroundframe from video
 	Mat frame, background;
@@ -96,7 +113,8 @@ int main()
 	//----------------
 	// set background frame
 	background = frame;
-
+	int waveCount = 0;
+	int skippedFrames = 0;
 
 	while (1)
 	{
@@ -114,23 +132,75 @@ int main()
 		// a) Read a stream of images from a web camera, and displaying the video
 		//----------------
 
-
+		skippedFrames = (skippedFrames+1)%frameSkipNum;
 
 
 		// destination frame
 		Mat frameDest, diff;
 		diff = Mat::zeros(frame.rows, frame.cols, CV_8UC1);
 		myFrameDifferencing(background, frame, diff);
-		imshow("background", background);
+		//imshow("background", background);
 		imshow("diff", diff);
 
 		Mat result;
 		result = Mat::zeros(diff.rows, diff.cols, CV_8UC1);
-		for (Mat templ : templates) {
+		for (int i = 0; i < templates.size(); i++) {
+			Mat templ = templates[i];
 			if (myTemplateMatching(frame, diff, templ, result) == true) {
-				namedWindow("something");
-				imshow("something", templ);
+				if (i < 3) {
+					cout << "okay" << endl;
+					putText(frame, "OKAY", Point(frame.rows, 100), FONT_HERSHEY_PLAIN, 3, Scalar(0,255,0),2,8);
+				}
+				else {
+					if (i < 6) {
+						cout << "peace" << endl;
+						putText(frame, "PEACE", Point(frame.rows, 100), FONT_HERSHEY_PLAIN, 3, Scalar(255, 0, 255), 2, 8);
+					}
+					else
+					{
+						cout << waveCount << endl;
+						if (i < 9) {
+							if (waveCount == 0)
+							{
+								//cout << "wave1" << endl;
+								waveCount++;
+							}
+							else {
+								putText(frame, "WAVE", Point(frame.rows, 100), FONT_HERSHEY_PLAIN, 3, Scalar(255, 255, 0), 2, 8);
+							}
+						}
+						else {
+							if (i < 12) {
+								if (waveCount == 0)
+								{
+									//cout << "wave2" << endl;
+									waveCount++;
+								}
+								else {
+									putText(frame, "WAVE", Point(frame.rows, 100), FONT_HERSHEY_PLAIN, 3, Scalar(255, 255, 0), 2, 8);
+								}
+							}
+							else {
+							
+								if (waveCount == 0)
+								{
+									//cout << "wave3" << endl;
+									waveCount++;
+								}
+								else {
+									putText(frame, "WAVE", Point(frame.rows, 100), FONT_HERSHEY_PLAIN, 3, Scalar(255, 255, 0), 2, 8);
+								}
+							}
+						}
+					}
+				}
 				break;
+			}
+			else {
+				if (skippedFrames = 0)
+				{
+					waveCount = 0;
+				}
 			}
 		}
 
@@ -200,9 +270,8 @@ bool myTemplateMatching(Mat& mainFrame, Mat& testFrame, Mat& templt, Mat& result
 	Point  minLoc, maxLoc, matchLoc;
 
 	minMaxLoc(results, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
-	if (maxVal > .6) {
+	if (maxVal > .65) {
 
-		cout << maxVal << endl;
 		matchLoc = maxLoc;
 		rectangle(mainFrame, matchLoc, Point(matchLoc.x + templt.cols, matchLoc.y + templt.rows), Scalar(0, 255, 0), 2, 8, 0);
 		return true;
@@ -213,7 +282,18 @@ bool myTemplateMatching(Mat& mainFrame, Mat& testFrame, Mat& templt, Mat& result
 
 vector<Mat> createTemplates(Mat& image) {
 	Mat temp, templ;
-	vector<double> scales = { 1, 1.25, .75 }; //1.33, .66, 1.5, .5 
+	vector<double> scales = { 1, 1.25, .75,.5}; //1.33, .66, 1.5, .5 
+	//vector<double> scales = { .25 };
+	//vector<double> scales = { .5 };
+	//vector<double> scales = {.75};
+	//vector<double> scales = { 1};
+	//vector<double> scales = {1.25};
+	//vector<double> scales = {1.5};
+	//vector<double> scales = { 1.75 };
+	//vector<double> scales = { 2 };
+
+
+
 	vector<Mat> templates;
 	for (double& scale : scales) {
 		resize(image, temp, Size(), scale, scale);
@@ -224,4 +304,19 @@ vector<Mat> createTemplates(Mat& image) {
 		templates.push_back(templ);
 	}
 	return templates;
+}
+
+//Function that accumulates the frame differences for a certain number of pairs of frames
+void myMotionEnergy(vector<Mat> mh, Mat& dst) {
+	Mat mh0 = mh[0];
+	Mat mh1 = mh[1];
+	Mat mh2 = mh[2];
+
+	for (int i = 0; i < dst.rows; i++) {
+		for (int j = 0; j < dst.cols; j++) {
+			if (mh0.at<uchar>(i, j) == 255 || mh1.at<uchar>(i, j) == 255 || mh2.at<uchar>(i, j) == 255) {
+				dst.at<uchar>(i, j) = 255;
+			}
+		}
+	}
 }
