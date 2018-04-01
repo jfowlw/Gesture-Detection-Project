b84c11cbb0bc7a171eb715497a3ad170ab084210
jrhill95 <32690216+jrhill95@users.noreply.github.com>
2018-04-01 13:19:54 -0400

    Add files via upload

--- CS 440 Project 2/CS440_640_Lab6_part2/CS440_640_Lab6_part2/Source.cpp
+++ CS 440 Project 2/CS440_640_Lab6_part2/CS440_640_Lab6_part2/Source.cpp
@@ -19,8 +19,6 @@
 using namespace cv;
 using namespace std;
 
-//Global variables
-Mat img; Mat templ;
 //function declarations
 
 /**
@@ -46,9 +44,18 @@ Function that detects whether a pixel belongs to the skin based on RGB values
 */
 void mySkinDetect(Mat& src, Mat& dst);
 
+void myFrameDifferencing(Mat& prev, Mat& curr, Mat& dst);
+
+bool myTemplateMatching(Mat& mainFrame, Mat& testFrame, Mat& templt, Mat& results);
+
+vector<Mat> createTemplates(Mat& image);
+
+//global 
+Mat okay = imread("okay-hand.jpg", IMREAD_COLOR); // Read the file
+
 int main()
 {
-	
+
 	//----------------
 	//a) Reading a stream of images from a webcamera, and displaying the video
 	//----------------
@@ -63,23 +70,34 @@ int main()
 		return -1;
 	}
 
-	Mat image, temp, result;
-	image = imread("okay-hand.jpg", IMREAD_COLOR); // Read the file
-	//image2 = imread("okay-hand3.jpg", IMREAD_COLOR);
-	Size size(image.cols / 2, image.rows / 2);
-	resize(image, temp, size);
-	// destination image
-	templ = Mat::zeros(temp.rows, temp.cols, CV_8UC1); //Returns a zero array of same size as src mat, and of type CV_8UC1
-	//img = Mat::zeros(image2.rows, image2.cols, CV_8UC1);
-	mySkinDetect(temp, templ);
+	vector<Mat> templates = createTemplates(okay);
 	//imshow("templ", templ);
 	//create a window called "MyVideo"
 	namedWindow("MyVideo0", WINDOW_AUTOSIZE);
-	//create a window called "Skin" for skin color detection
-	namedWindow("Skin", WINDOW_AUTOSIZE);
-	//namedWindow("Contours", CV_WINDOW_AUTOSIZE);
-	namedWindow("Results", WINDOW_AUTOSIZE);
 	RNG rng(12345);
+
+
+	// read a backgroundframe from video
+	Mat frame, background;
+	bool bSuccess = cap.read(frame);
+
+	//if not successful, break loop
+	if (!bSuccess)
+	{
+		cout << "Cannot read a frame from video stream" << endl;
+	}
+	//----------------
+	// a) Read a stream of images from a web camera, and displaying the video
+	//----------------
+
+
+	//----------------
+	//	b) Skin color detection
+	//----------------
+	// set background frame
+	background = frame;
+
+
 	while (1)
 	{
 		// read a new frame from video
@@ -95,72 +113,29 @@ int main()
 		//----------------
 		// a) Read a stream of images from a web camera, and displaying the video
 		//----------------
-		
 
-		//----------------
-		//	b) Skin color detection
-		//----------------
+
+
 
 		// destination frame
-		Mat frameDest;
-		frameDest = Mat::zeros(frame.rows, frame.cols, CV_8UC1); //Returns a zero array of same size as src mat, and of type CV_8UC1
-		mySkinDetect(frame, frameDest);
-		result = Mat::zeros(frame.rows, frame.cols, CV_8UC1);
+		Mat frameDest, diff;
+		diff = Mat::zeros(frame.rows, frame.cols, CV_8UC1);
+		myFrameDifferencing(background, frame, diff);
+		imshow("background", background);
+		imshow("diff", diff);
 
-		
-		
-
-		//int erosion_size = 3;
-		//Mat element = getStructuringElement(MORPH_RECT,
-		//	Size(2 * erosion_size + 1, 2 * erosion_size + 1),
-		//	Point(erosion_size, erosion_size)
-		//);
-
-		//erode(frameDest, frameDest, element); 
-		//erode(frameDest, frameDest, element);
-		//dilate(frameDest, frameDest, element);
-		//dilate(frameDest, frameDest, element);
-		/*
-		vector<vector<Point> > contours;
-		vector<Vec4i> hierarchy;
-		findContours(frameDest, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
-		Mat drawing = Mat::zeros(frameDest.size(), CV_8UC3);
-
-		for (int i = 0; i< contours.size(); i++)
-		{
-			Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-			drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
+		Mat result;
+		result = Mat::zeros(diff.rows, diff.cols, CV_8UC1);
+		for (Mat templ : templates) {
+			if (myTemplateMatching(frame, diff, templ, result) == true) {
+				namedWindow("something");
+				imshow("something", templ);
+				break;
+			}
 		}
 
-		/// Show in a window
-		
-		imshow("Contours", drawing);*/
 
-
-		
-			
-		matchTemplate(frameDest, templ, result, CV_TM_CCOEFF_NORMED);
-		//matchTemplate(frame, temp, result, CV_TM_SQDIFF_NORMED);
-		//normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
-		double minVal, maxVal;
-		Point  minLoc, maxLoc, matchLoc;
-
-		minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
-		cout << maxVal << endl;
-		if (maxVal > .6) {
-
-
-			matchLoc = maxLoc;
-
-			rectangle(result, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar(200, 200, 0), 2, 8, 0);
-			rectangle(frame, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
-			}
-
-			
-		
 		imshow("MyVideo0", frame);
-		imshow("Skin", frameDest);
-		imshow("Results", result);
 		//wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
 		if (waitKey(30) == 27)
 		{
@@ -194,14 +169,59 @@ void mySkinDetect(Mat& src, Mat& dst) {
 	//Surveys of skin color modeling and detection techniques:
 	//Vezhnevets, Vladimir, Vassili Sazonov, and Alla Andreeva. "A survey on pixel-based skin color detection techniques." Proc. Graphicon. Vol. 3. 2003.
 	//Kakumanu, Praveen, Sokratis Makrogiannis, and Nikolaos Bourbakis. "A survey of skin-color modeling and detection methods." Pattern recognition 40.3 (2007): 1106-1122.
-	for (int i = 0; i < src.rows; i++){
-		for (int j = 0; j < src.cols; j++){
+	for (int i = 0; i < src.rows; i++) {
+		for (int j = 0; j < src.cols; j++) {
 			//For each pixel, compute the average intensity of the 3 color channels
 			Vec3b intensity = src.at<Vec3b>(i, j); //Vec3b is a vector of 3 uchar (unsigned character)
 			int B = intensity[0]; int G = intensity[1]; int R = intensity[2];
-			if ((R > 95 && G > 40 && B > 20) && (myMax(R, G, B) - myMin(R, G, B) > 15) && (abs(R - G) > 15) && (R > G) && (R > B)){
+			if ((R > 95 && G > 40 && B > 20) && (myMax(R, G, B) - myMin(R, G, B) > 15) && (abs(R - G) > 15) && (R > G) && (R > B)) {
 				dst.at<uchar>(i, j) = 255;
 			}
 		}
 	}
+}
+
+
+//Function that does frame differencing between the current frame and the previous frame
+void myFrameDifferencing(Mat& prev, Mat& curr, Mat& dst) {
+	absdiff(prev, curr, dst);
+	Mat cpy = dst.clone();
+	cvtColor(dst, cpy, COLOR_BGR2GRAY);
+	dst =
+		cpy > 50;
+}
+
+
+bool myTemplateMatching(Mat& mainFrame, Mat& testFrame, Mat& templt, Mat& results) {
+	matchTemplate(testFrame, templt, results, CV_TM_CCOEFF_NORMED);
+	//matchTemplate(frame, temp, result, CV_TM_SQDIFF_NORMED);
+	//normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
+	double minVal, maxVal;
+	Point  minLoc, maxLoc, matchLoc;
+
+	minMaxLoc(results, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+	if (maxVal > .6) {
+
+		cout << maxVal << endl;
+		matchLoc = maxLoc;
+		rectangle(mainFrame, matchLoc, Point(matchLoc.x + templt.cols, matchLoc.y + templt.rows), Scalar(0, 255, 0), 2, 8, 0);
+		return true;
+	}
+	return false;
+}
+
+
+vector<Mat> createTemplates(Mat& image) {
+	Mat temp, templ;
+	vector<double> scales = { 1, 1.25, .75 }; //1.33, .66, 1.5, .5 
+	vector<Mat> templates;
+	for (double& scale : scales) {
+		resize(image, temp, Size(), scale, scale);
+		// destination image
+
+		templ = Mat::zeros(temp.rows, temp.cols, CV_8UC1); //Returns a zero array of same size as src mat, and of type CV_8UC1
+		mySkinDetect(temp, templ);
+		templates.push_back(templ);
+	}
+	return templates;
 }
